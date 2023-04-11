6a2213d66139d166cda1d14fee980a17e8abfdef
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-04-11 15:02:27 +0100

    prisma setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <main>
-      <div>Home</div>
+    <main className='py-8 px-48'>
+     <Link className='bg-teal-700 text-black font-medium py-2 px-4 rounded-md' href={"/dashboard"}>Go to the Dashboard</Link>
     </main>
   );
 }
